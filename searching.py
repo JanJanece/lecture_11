4cc1a5f1650c85161023f7615fbf066bc9b91492
JanJanece <226390@vutbr.cz>
2021-04-20 17:20:35 +0200

    bla

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,11 +13,31 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    if field not in {"unordered_numbers", "ordered_numbers", "dna_sequence"}:
+        return None
+
+    with open(file_path, "r") as json_file:
+        dict = json.load(json_file)
+
+    return dict[field]
+
+
+def linear_search(seq, num):
+    count = 0
+    list_of_positions = []
+    for idx, number in enumerate(seq):
+        if number == num:
+            count += 1
+            list_of_positions.append(idx)
+    return {"positions": list_of_positions, "count": count}
 
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
+    result_1 = linear_search(sequential_data, 13)
+    print(result_1)
 
 if __name__ == '__main__':
     main()
